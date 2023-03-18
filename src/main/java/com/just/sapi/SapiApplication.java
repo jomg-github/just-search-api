2a15d0d431d5d710e9bb8063f8175404b637b7dd
조민기 <jomg@jomgui-MacBookPro.local>
2023-03-18 11:34:18 +0900

    init

---
+++ src/main/java/com/just/sapi/SapiApplication.java
@@ -0,0 +1,13 @@
+package com.just.sapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/just/sapi/SapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.sapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
